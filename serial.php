b496eb81b397811ab483358f3386a43e24e4b3f2
Estresban <101512042+Estresban@users.noreply.github.com>
2022-05-22 12:17:55 +0200

    Update serial.php

--- serial.php
+++ serial.php
@@ -3,11 +3,11 @@
     //$puerto ="COM4";
     $puerto = "/dev/ttyUSB0";
     $permiso = "r+";
-        //if (!$conect) {$_SESSION['estacon'] = "0";} 
+	$Sercon = fopen ($puerto, $permiso);
+        //if (!$Sercon) {$_SESSION['estacon'] = "0";} 
 	      //else {$_SESSION['estacon'] = "1";}
 	
- 	$fcon = fopen ($puerto, $permiso);
-        if (!$fcon) { $estacon = "0";} 
+        if (!$Sercon) { $estacon = "0";} 
 	    	else {$estacon = "1";}
 	sleep(1);
  
