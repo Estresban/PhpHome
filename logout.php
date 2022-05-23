f8514fdc5fdc09a753027ea1648dd769f5d86197
Estresban <101512042+Estresban@users.noreply.github.com>
2022-05-23 10:43:24 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,13 +3,13 @@
 include("conexion.php");
 
 $fecha = date("Y-m-d H:i:s");
-$id_usuario=1;//$_SESSION['id-usuario-logueado'];
+$id_usuario=$_SESSION['id-usuario-logueado'];
 $comando='deslogueado';
-$query2 = "INSERT INTO historial(id_usuario, id_comando, comando, fecha, otros)VALUES ('$id_usuario',2,'$comando','$fecha',NULL)";
+$id_comando='2';
+$query2 = "INSERT INTO historial(id_usuario, id_comando, comando, fecha, otros)VALUES ('$id_usuario',$id_comando,'$comando','$fecha',NULL)";
 $mysqli->query($query2);
 
-//session_destroy();
-
-//header("Location:index.php");
+session_destroy();
+header("Location:index.php");
 
 ?>
