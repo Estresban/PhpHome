1afb8c86ac0d46ff200ed6dfd9c7c464845a0684
Estresban <101512042+Estresban@users.noreply.github.com>
2022-05-23 10:56:38 +0200

    Update consultaestados.php

--- consultaestados.php
+++ consultaestados.php
@@ -3,6 +3,10 @@ include ('conexion.php');
 	$query9 = "SELECT escalera,salon,pasillo,cocina,patio,matrimonio,hab1,hab2,hab3,garaje FROM estadosp2 WHERE fecha=(select (MAX(fecha)) from estadosp2 WHERE fecha)"; 
 	$consulta = $mysqli->query($query9);
 	$estaBBDD = $consulta->fetch_assoc();
+
+$query10 = "SELECT est0,est1,est2,est3,est4,est5,est6,est7,est8,est9,est10 FROM estadosp1 WHERE fecha=(select (MAX(fecha)) from estadosp1 WHERE fecha)"; 
+	$consulta = $mysqli->query($query10);
+	$estaBBDD1 = $consulta->fetch_assoc();
  
  if($estaBBDD);{
     $escalera=$estaBBDD['escalera'];
@@ -16,5 +20,19 @@ include ('conexion.php');
     $hab3=$estaBBDD['hab3'];
     $garaje=$estaBBDD['garaje'];
     }
+ 
+ if($estaBBDD1);{
+    $motor1=$estaBBDD1['escalera'];
+    $persiana1=$estaBBDD1['salon'];
+    $persiana2=$estaBBDD['pasillo'];
+    $aireacondicionado=$estaBBDD1['cocina'];
+    $calefaccion=$estaBBDD1['patio'];
+    $alarma=$estaBBDD1['matrimonio'];
+    $hab1=$estaBBDD1['hab1'];
+    $hab2=$estaBBDD1['hab2'];
+    $hab3=$estaBBDD1['hab3'];
+    $garaje=$estaBBDD1['garaje'];
+    }
     //var_dump ($estaBBDD);
     ?>
+1
